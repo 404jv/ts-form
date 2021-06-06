7c5f4c2704d69d11809774ec3bf02cc2971a26a0
João Victor <joaovictorramalho7@gmail.com>
2021-06-06 19:27:25 -0300

    Create app

---
+++ backend/src/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import 'reflect-metadata';
+import { AppError } from './errors/AppError';
+import './database/connection';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((error: Error, req: Request, res: Response, _next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json(error.message);
+  }
+
+  return res.status(500).json({
+    status: "Error",
+    message: `Internal server error ${error.message}`
+  });
+});
+
+export default app;
